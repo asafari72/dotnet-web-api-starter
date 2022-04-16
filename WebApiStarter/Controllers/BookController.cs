517e77f383452c9db4c5ffda4a2ea38db064b246
asafari72 <asafari72@gmail.com>
2022-04-16 17:05:03 +0430

    Add project files.

---
+++ WebApiStarter/Controllers/BookController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiStarter.Models;
+using WebApiStarter.Services;
+
+namespace WebApiStarter.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookController : ControllerBase
+    {
+        private readonly BooksService _booksService;
+
+        public BookController(BooksService booksService) =>
+       _booksService = booksService;
+
+
+        [HttpGet]
+        public async Task<List<Book>> Get() => await _booksService.GetAsync();
+
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Book newBook)
+        {
+            await _booksService.CreateAsync(newBook);
+            return CreatedAtAction(nameof(Get), new { id = newBook.Id }, newBook);
+        }
+
+    }
+}
---
+++ WebApiStarter/Program.cs
@@ -0,0 +1,46 @@
+using JsonApiDotNetCore.Configuration;
+using MongoDB.Driver;
+using WebApiStarter.Models;
+using WebApiStarter.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.Configure<MongoDBSettings>(
+    builder.Configuration.GetSection("MongoDBSettings"));
+
+builder.Services.AddSingleton<BooksService>();
+
+
+// MiddleWares
+builder.Services.AddJsonApi();
+
+builder.Services.AddSingleton<IMongoDatabase>(options => {
+    var settings = builder.Configuration.GetSection("MongoDBSettings").Get<MongoDBSettings>();
+    var client = new MongoClient(settings.ConnectionString);
+    return client.GetDatabase(settings.DatabaseName);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
